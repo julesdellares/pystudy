90e1ba7b746d1bbb0064675151b6d38751717a0f
jules <101470183+julesdellares@users.noreply.github.com>
2022-07-04 11:27:44 -0700

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,23 @@
+# variable = a container for value, it acts like what it contains
+#variables have different data types
+
+# first_name = "Jules"
+# last_name = "del Lares"
+# full_name = first_name +" "+ last_name
+
+# print("Hello " +full_name)
+
+
+age = "28"
+#age += 1
+print(age)
+print("Your age is: "+str(age))
+#print(type(age))
+
+
+height = 250.50
+print("your height is: " +str(height)+"cm")
+print(type(height))
+
+human = True
+print(human)
